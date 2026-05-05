eaf1988a2c31c648a41947ec88765b3270330ae7
jamin cheon <cscsh2068@naver.com>
2026-05-05 15:35:30 +0900

    feat: 로그인 요청 및 응답 DTO 추가

---
+++ src/main/java/com/example/springboot_realtimechat/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.example.springboot_realtimechat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class LoginResponse {
+    //  Authorization: `${tokenType} ${accessToken}`
+    private final String tokenType = "Bearer"; //프론트에서 Authorization 헤더 만들 때 의미가 명확해짐
+    private final String accessToken;
+
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
